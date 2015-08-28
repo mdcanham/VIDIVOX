26fe57fffee8d28fd1444f0eefdaa6e4ceb851cc
Matthew Canham <mdcanham@me.com>
2015-08-28 15:32:05 +1200

    Removed code that we were using to test git

--- src/com/videvox/app.java
+++ src/com/videvox/app.java
@@ -1,18 +1,12 @@
 package com.videvox;
 
 /**
- * Created by matthewcanham on 28/08/15.
+ * 
  */
 public class app {
 
-    private static String names = "Matt and Jayden";
-
     public static void main(String[] args){
         System.out.println("Hello, world!");
-        System.out.println("This is anoher git test!");
-        talk();
     }
-    public static void talk(){
-        System.out.println("Talk");
-    }
+
 }
