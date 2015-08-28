6bbce8cf695729f129250083979f75a03d1d8832
Matthew Canham <mdcanham@me.com>
2015-08-28 12:18:50 +1200

    Initial commit

---
+++ src/com/videvox/app.java
@@ -0,0 +1,10 @@
+package com.videvox;
+
+/**
+ * Created by matthewcanham on 28/08/15.
+ */
+public class app {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
+}
