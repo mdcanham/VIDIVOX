973541fb006480c98c73598e6fcedbe9c7972432
Matthew Canham <mdcanham@me.com>
2015-08-28 13:22:05 +1200

    Added changes to test git with Jayden

--- src/com/videvox/app.java
+++ src/com/videvox/app.java
@@ -4,7 +4,11 @@ package com.videvox;
  * Created by matthewcanham on 28/08/15.
  */
 public class app {
+
+    private static String names = "Matt and Jayden";
+
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        System.out.println("This is anoher git test!");
     }
 }
